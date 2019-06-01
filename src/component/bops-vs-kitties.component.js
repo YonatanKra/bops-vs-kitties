24a03e982a7628a393f16b290290d7e939d69101
Yonatan Kra <yonatan.kr@walkme.com>
2019-06-01 10:24:38 +0300

    Remove demo functions

--- src/component/bops-vs-kitties.component.js
+++ src/component/bops-vs-kitties.component.js
@@ -1,13 +1,9 @@
 // setup a template
 import * as templateString from './bops-vs-kitties.component.html';
-import * as BOP_IMAGE from '../assets/bop.png';
 
 // setup a template element with our template
 const template = document.createElement('template');
 template.innerHTML = templateString;
-
-const BOP_IMAGE_ELEMENT = new Image(50, 50);
-BOP_IMAGE_ELEMENT.src = BOP_IMAGE;
 
 export class BopsVsKitties extends HTMLElement {
     constructor() {
@@ -16,44 +12,9 @@ export class BopsVsKitties extends HTMLElement {
         this.root.appendChild(template.content.cloneNode(true));
         this.canvas = this.root.querySelector('canvas');
         this.ctx = this.canvas.getContext('2d');
-        this.canvas.addEventListener('click', (event) => {
-            this.collisionDetection(event.offsetX, event.offsetY);
-            this.drawBallOnClick(event.offsetX, event.offsetY);
-        });
-        this.render();
-    }
-    
-    drawBall() {
-        this.x = this.x ? this.x + 1 : 1;
-        this.y = this.y ? this.y + 1 : 1;
-        // add a circle
-        this.ctx.beginPath();
-        this.ctx.arc(this.x, this.y, 15, 0, Math.PI * 2); //x, y, radius, start angle, end angle
-        this.ctx.fillStyle = '#0095DD'; // just fill it
-        this.ctx.fill();
-        this.ctx.closePath();
-    }
- 
-    drawBallOnClick(x, y) {
-        // add a circle
-        this.ctx.beginPath();
-        this.ctx.arc(x, y, 15, 0, Math.PI * 2); //x, y, radius, start angle, end angle
-        this.ctx.fillStyle = '#0095DD'; // just fill it
-        this.ctx.fill();
-        this.ctx.closePath();
-    }
-
-    collisionDetection(x, y) {
-        if (x < this.x + 15 && x > this.x - 15
-            && y < this.y + 15 && y > this.y - 15) {
-                alert('Hit!!!');
-            }
     }
 
     render() {
         requestAnimationFrame(() => this.render());
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        // draw the ball
-        this.drawBall();
     }
 }
